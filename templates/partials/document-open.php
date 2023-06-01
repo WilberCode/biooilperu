d96b45ac1481917e6e263b34049154ded64c3326
wilber <parionahuaraca@gmail.com>
2023-05-31 20:49:20 -0500

    cambios

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -7,7 +7,21 @@
     <title><?= wp_get_document_title() ?></title>  
     <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
      
-    <?php wp_head() ?>  
- 
+    <?php wp_head();?>  
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-N83HVR51RJ"></script>
+    <script>
+        window.onload = function(){
+        setTimeout(function(){
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-N83HVR51RJ'); 
+          },3000)
+        }
+    </script>
+  
 </head>
 <body <?php body_class()  ?> > 
