9e84dd67773c458fa92ec42202aed821357d5574
wilber <parionahuaraca@gmail.com>
2023-07-20 20:34:38 -0500

    cambios

---
+++ page-landing-farmacias.php
@@ -0,0 +1,96 @@
+<?php
+/*
+Template Name: Landing Farmacias
+Template Post Type: post, page, event
+*/  ?>
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Cyber WOW - Bio Oil Perú</title>
+      <meta name="description" content="Compra Bio-Oil en tu tienda favorita.">
+      
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-N83HVR51RJ"></script>
+      <script>
+         window.onload = function(){
+         setTimeout(function(){
+               window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+
+               gtag('config', 'G-N83HVR51RJ'); 
+            },2000)
+         }
+      </script>
+      <!-- Google Tag Manager -->
+   <script>
+   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+   })(window,document,'script','dataLayer','GTM-MT4BDFH');
+   
+   </script>
+   <!-- End Google Tag Manager -->
+  
+      <style>
+      
+         html{box-sizing:border-box;margin:0;padding:0}*,:after,:before{box-sizing:inherit;margin:inherit;padding:inherit}img{display:block}.flex{display:flex}.justify-center{justify-content:center}.items-start{align-items:center}.w-full{width:100%}
+         .max-w-678{max-width:678px} 
+         .max-w-1356{max-width:1356px} 
+         .landing-link{
+            font-size: 0;
+            display: block;
+            margin:0;
+         } 
+         img{
+            margin:0;
+         }
+         header,footer,.wpadminbar{
+         display: none;
+         }
+      </style>
+    </head>
+
+<body>
+ <div  class="flex justify-center items-start  "> 
+   <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MT4BDFH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+         <main> 
+           <div >
+             <img  class=" w-full max-w-1356"  src="<?=home_url();?>/wp-content/themes/biooil/landing/farmacias/titulo.jpg" alt="farmacias">
+            </div>
+            <div class="flex justify-center  items-center w-full " >
+           
+               <div>
+                  <a class="landing-link" id="landing-inkafarma" target="_blank"  href="https://inkafarma.pe/buscador?keyword=bio%20oil">
+                     <img class="  w-full max-w-678" src="<?=home_url();?>/wp-content/themes/biooil/landing/farmacias/inkafarma.jpg" alt="Inkafarma">
+                     Inkafarma
+                  </a>
+               </div> 
+               <div>
+                  <a class="landing-link" id="landing-mifarma" target="_blank"  href="https://www.mifarma.com.pe/buscador?keyword=bio%20oil"> 
+                     <img class=" w-full max-w-678" src="<?=home_url();?>/wp-content/themes/biooil/landing/farmacias/mifarma.jpg?v=5" alt="Mifarma">
+                     Mifarma
+                  </a>
+
+               </div>
+            </div> 
+            <div>
+             <img  class=" w-full max-w-1356"  src="<?=home_url();?>/wp-content/themes/biooil/landing/farmacias/gel-bio-oil.jpg" alt="get bio oil">
+            </div>
+
+         </main >
+
+      </div>  
+
+      </body>
+</html>
