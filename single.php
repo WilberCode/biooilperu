a5be3aa99deddf2b26d36e7e5d712de2ebc8ee70
wilber <parionahuaraca@gmail.com>
2023-09-26 20:33:51 -0500

    cambios

--- single.php
+++ single.php
@@ -30,7 +30,52 @@
             ?>  
           </div>
       
+          <div  class="hidden sm:block" >
+            <h3 class="text-[24px] mt-[25px] mb-[10px]">Más publicaciones</h3>
+            <div  class=" grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-x-[15px] sm:gap-x-[30.69px] gap-y-[30px] place-content-center    " >
+            <?php 
+                    $args_relacionado = array( 
+                    'post_type' =>get_post_type( get_the_ID()),  
+                    'orderby' => 'comment_count',
+                    'order' => 'desc',
+                    'post__not_in' => array(get_the_ID()),
+                    'posts_per_page' => 3
+                    );
+
+
+                    $listing = new WP_query($args_relacionado); 
+                    // create output
+                    if ($listing->have_posts()) : 
+                      while ($listing->have_posts()) : $listing->the_post();
           
+                                      $arr_image = thumbnail_image_url('homepage-thumbnail'); 
+                                          if ($arr_image!= '' ) :
+                                              $url_image = $arr_image;
+                                      else :
+                                              $url_image = get_template_directory_uri()."/build/img/thumbnail-default.jpg";
+                                      endif;  
+                                    
+                                      ?> 
+
+                                      <a class="card" href="<?=get_permalink()?>" >
+                                        <img  src="<?=$url_image?>" alt="<?=get_the_title()?>">   
+                                        <div  class=" card-body " > 
+                                          <h2 class=" !text-[18px] sm:!text-[22px] !leading-[27.47px] tracking-normal " > <?=get_the_title()?></h2>
+                                        </div>
+                                        <div  class=" card-footer  ">
+                                          <button>QUIERO LEER MÁS</button>
+                                        </div>
+                                      </a> 
+                    <?php	endwhile;
+                    else: ?>
+                        <p  class="mt-2" >Por ahora no tenemos más publicaciones.</p>
+                    <?php  endif; 
+                    // reset the query
+                    wp_reset_postdata();
+                ?>
+        </div>
+        </div>
+
     </main> 
     <aside class="mx-auto w-full max-w-[320px] blog-sidebar mt-10 sm:mt-17 "> 
         <div class="mb-8"> 
@@ -82,8 +127,7 @@
                 </div>
             </div>    
         </aside> 
-</div>
-
+</div> 
  
 <!-- <code>
   <pre>
